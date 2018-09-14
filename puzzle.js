224e905ac8d96e1e220f639f8089ec6ab67c0c23
Jace Roldan <jtroldan@up.edu.ph>
2018-09-15 06:51:53 +0800

    Add checking layer

--- puzzle.js
+++ puzzle.js
@@ -1,14 +1,32 @@
-var arr = [];
+var arr;
+var lastLayer;
 var main = document.getElementById('main');
+var levels;
+var answerLevel = [];
+reset();
+var answerIsCorrect = false;
 
-function run() {
+function removeAllChildNodes(node) {
+	while(node.firstChild) {
+		node.removeChild(node.firstChild);
+	}
+}
+
+function initialize() {
 	main.className = "main";
+	answerLevel = [];
+	levels = Math.floor(Math.random() * 20 + 2);
+	arr = [];
+	lastLayer = [];
 	for (let i = 0; i < 20; i++) {
 		arr.push(0);
+		answerLevel.push(false);
 	}
 
 	arr[arr.length/2] = 1;
+}
 
+function run(limit) {
 	let screen = [];
 
 	screen.push(0)
@@ -19,7 +37,7 @@ function run() {
 
 	printRule30Array();
 
-	while(passes < arr.length) {
+	while(passes < limit) {
 		let lb = 1;
 		let replacement = arr.slice(0);
 
@@ -46,7 +64,13 @@ function run() {
 	}
 }
 
-// console.log("Finished");
+function reset() {
+	removeAllChildNodes(main);
+	initialize()
+	run(levels);
+	addBlankLayer();
+	addEventListenersToLastLayer();
+}
 
 function printRule30Array(passes) {
 	for(let i = 0; i < arr.length; i++) {
@@ -67,16 +91,85 @@ function printRule30Array(passes) {
 			style += " border-bottom: 1px solid; "
 		}
 
-		if(i === 0) {
-			console.log(style);
-		}
 		div.setAttribute("style", style);
-
+		div.className = "boxes";
 		main.append(div);
 	}
-	console.log("");
 }
 
+function addBlankLayer() {
+	for(let i = 0; i < arr.length; i++) {
+		let style = "";
+		let div = document.createElement("DIV");
+		div.className = "boxes answer";
+
+		if(i == 0) {
+			style += "border-left: 1px solid;";
+		}
+
+		style += "border-bottom: 1px solid";
+
+		div.setAttribute("style", style);
+		main.append(div);
+		lastLayer.push(div);
+	}
+}
+
+function addEventListenersToLastLayer() {
+	for(let i = 0; i < lastLayer.length; i++) {
+		lastLayer[i].addEventListener("click", function() {
+			if(lastLayer[i].style.backgroundColor === "black") {
+				lastLayer[i].style.backgroundColor = "";
+				answerLevel[i] = false;
+			} else {
+				lastLayer[i].style.backgroundColor = "black";
+				answerLevel[i] = true;
+			}
+		});
+	}
+}
+
+function checkAnswer() {
+	let flag = true;
+	for(let i = 0; i < arr.length; i++) {
+		let arg1, arg2, arg3;
+		if(i == 0) {
+			arg1 = 0;
+			arg3 = arr[i+1];
+		} else if (i == arr.length - 1) {
+			arg1 = arr[i-1];
+			arg3 = 0;
+		} else {
+			arg1 = arr[i-1];
+			arg3 = arr[i+1];
+		}
+		arg2 = arr[i];
+
+		correctAnswer = arg1 ^ (arg2 | arg3);
+		givenAnswer = answerLevel[i];
+
+		console.log(arg1 + " " + arg2 + " " + arg3);
+		console.log(correctAnswer + " : " + givenAnswer);
+		if(correctAnswer != givenAnswer) {
+			flag = false;
+			break;
+		}
+	}
+
+	message = (flag) ? "Correct" : "Wrong";
+
+	alert(message);
+
+	if(flag) {
+		triggerTextBoxes();
+	}
+
+	return flag;
+}
+
+function triggerTextBoxes() {
+
+}
 /*
 	111
 	110
